ac1be9cde2c8c6ced5e7c258dc875c33c90b4949
Olivia Di Matteo <dimatteo.olivia@gmail.com>
2017-06-16 10:45:58 -0400

    Replaced activation functions with tanh to obtain avg fidelity of 94.4. Trying some different bases and then moving on to two qubit case.

--- neuralnet_lbmle.py
+++ neuralnet_lbmle.py
@@ -7,7 +7,10 @@ from scipy.linalg import sqrtm
 from keras.layers import Input, Dense
 from keras.models import Sequential
 
+import keras.backend as K
+
 from generate_training_data import *
+
 
 def train_nn(train_in, train_out, hidden_layer_size):
     """ Build and train a neural network using the 'experimental data'.
@@ -30,7 +33,7 @@ def train_nn(train_in, train_out, hidden_layer_size):
     model.add(Dense(hidden_layer_size, activation = "tanh", input_shape = i_shape))
     
     # Add the output layer; need only one node that outputs a vector
-    model.add(Dense(len(train_out[0]), activation = "softmax")) 
+    model.add(Dense(len(train_out[0]), activation = "tanh")) 
     
     # Cosine proximity gives us a measure of the angle between the true/predicted
     # values on the Bloch sphere.
@@ -45,11 +48,13 @@ def train_nn(train_in, train_out, hidden_layer_size):
 N_TRIALS = 10000
 
 f = GaloisField(2)
-train_in, train_out, test_in, test_out, lbmle_freqs = generate_data(N_TRIALS, 0.1, f, eigenvectors, [0, 1], True)
+bases = [0, -1]
+train_in, train_out, test_in, test_out, lbmle_freqs = generate_data(N_TRIALS, 0.1, f, eigenvectors, bases, True)
 
 hidden_layer_sizes = [64]
 results_nn = []
 actual_test_mats = []
+fidelities = []
 
 for size in hidden_layer_sizes:
     print("Training neural network: ")
@@ -58,7 +63,6 @@ for size in hidden_layer_sizes:
     predictions = my_nn.predict(test_in)
     scaled_predictions = [p / np.linalg.norm(p) for p in predictions]
 
-    fidelities = []
     for i in range(len(test_in)):
         test_mat = (0.5*np.eye(2)) + 0.5*np.sum([test_out[i][j] * op_basis[j] for j in range(3)], 0)
         pred_mat = (0.5*np.eye(2)) + 0.5*np.sum([scaled_predictions[i][j] * op_basis[j] for j in range(3)], 0)
@@ -68,10 +72,12 @@ for size in hidden_layer_sizes:
         fidelities.append(qt.fidelity(qt.Qobj(test_mat), qt.Qobj(pred_mat)))
 
         if i in range(25, 30):
-            #print("Actual matrix")
-            #print(test_mat)
+            print("Actual matrix")
+            print(test_mat)
+            print(test_out[i])
             print("NN predicted matrix")
             print(pred_mat)
+            print(scaled_predictions[i])
             #print("Fidelity")
             #print(qt.fidelity(qt.Qobj(test_mat), qt.Qobj(pred_mat)))
 
@@ -82,7 +88,7 @@ results_lbmle = []
 mle = LBMLE(MUBs(f), eigenvectors)
 
 for i in range(len(lbmle_freqs)):
-    mle_res = mle.estimate([0, 1], lbmle_freqs[i])
+    mle_res = mle.estimate(bases, lbmle_freqs[i])
     fid = qt.fidelity(qt.Qobj(mle_res[0]), qt.Qobj(actual_test_mats[i]))
     results_lbmle.append(fid) 
     """
@@ -94,6 +100,11 @@ for i in range(len(lbmle_freqs)):
         print("Fidelity")
         print(fid)"""
     
+with open("fids_dim2.csv", "w") as outfile:
+    writer = csv.writer(outfile)
+    writer.writerow(["NN", "LBMLE"])
+    for row in zip(fidelities, results_lbmle):
+        writer.writerow(row)
 
 for res in results_nn:
     print("Hidden layer size: " + str(res[0]) + " Avg fidelity = " + str(res[1]))
