b53a5edca90864d7d166fb0b4f9a69f7590b0995
Olivia Di Matteo <dimatteo.olivia@gmail.com>
2017-07-03 06:03:33 -0400

    Merged eigenvectors into a single file.

--- eigvecs_dim2.py
+++
@@ -1,6 +0,0 @@
-import numpy
-from math import sqrt
-
-eigenvectors = [[ [1., 0], [0, 1.]],
-                [ [1./sqrt(2), 1j / sqrt(2)], [1./sqrt(2), -1j / sqrt(2)]],
-                [ [1./sqrt(2), 1./sqrt(2)], [1./sqrt(2), -1./sqrt(2)]]]
